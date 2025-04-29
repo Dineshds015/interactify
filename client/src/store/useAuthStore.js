b9be76758e3abd5ab9494744800b8dbc8df9b880
Dinesh Suthar <dineshds015@gmail.com>
2025-04-29 09:36:30 +0530

    Complete authentication with SignUp and Login

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -20,6 +20,7 @@ export const useAuthStore = create((set) => ({
             set({ isCheckingAuth: false })
         }
     },
+
     signup: async (data) => {
         set({ isSigningUp: true });
         try {
@@ -33,6 +34,20 @@ export const useAuthStore = create((set) => ({
             set({ isSigningUp: false });
         }
     },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("auth/login", data);
+            set({ authUser: res.data })
+            toast.success("Logged in successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: true });
+        }
+    },
+
     logout: async () => {
         try {
             await axiosInstance.post("/auth/logout");
